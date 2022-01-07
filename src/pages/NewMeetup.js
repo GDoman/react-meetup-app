84c2aafdbfe46232de6fc4cf5960e4f02a723c3a
Doman Gaul <doman.gaul@clurgo.com>
2022-01-07 14:46:22 +0100

    second and final part of the project

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,29 @@
+import { useHistory } from "react-router-dom";
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  const history = useHistory();
+
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-meetups-project-6097c-default-rtdb.europe-west1.firebasedatabase.app/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      history.replace("/");
+    });
+  }
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm></NewMeetupForm>
+      <NewMeetupForm onAddMeetup={addMeetupHandler}></NewMeetupForm>
     </section>
   );
 }
